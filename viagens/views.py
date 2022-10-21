9988ea07c4fd9864558b4fb16ad2604dd3c71423
TiagoSilvaGomes <tiagogomesblogger@gmail.com>
2022-10-20 21:25:29 -0300

    teste2

--- viagens/views.py
+++ viagens/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from viagens.forms import ViagemForms
+from django import forms
+
 
 def index(request):
     form= ViagemForms()
@@ -11,5 +13,17 @@ def revConsulta(request):
         form = ViagemForms(request.POST)
         contexto={'form': form}
         return render(request, 'consulta.html', contexto)
+
+def revConsulta(request):
+    if request.method == 'POST':
+        form = ViagemForms(request.POST)
+        if form.is_valid():
+            contexto={'forms': form}
+            return render(request, 'consulta.html', contexto)
+
+        else:
+            print('Form Inválido')
+            contexto = {'form': form}
+            return render(request, 'index.html', contexto)
 
 # Create your views here.
